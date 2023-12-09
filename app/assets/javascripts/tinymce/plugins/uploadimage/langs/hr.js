b1613db0ab5127d96bba1f01277e7e319418b7c5
Luka Bak <bakluka@gmail.com>
2023-12-09 22:23:21 +0100

    Translate to hr

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/hr.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('hr', {
+    'Insert an image from your computer': 'Umetnite sliku sa svog računala',
+    'Insert image': 'Umetnite sliku',
+    'Choose an image': "Odaberite sliku",
+    'You must choose a file': "Morate odabrati sliku",
+    'Got a bad response from the server': "Greška na poslužitelju",
+    "Didn't get a response from the server": "Nema odgovora od poslužitelja",
+    'Insert': "Umetni",
+    'Cancel': "Odustani",
+    'Image description': "Opis slike",
+});
